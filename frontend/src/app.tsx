0fd4063306d962caf3b06622f7fa58f324ae2d57
Athrun86 <la228004@student.helha.be>
2025-11-11 12:52:13 +0100

    Add Basename to router

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -81,7 +81,7 @@ function AppContent() {
  */
 export default function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/WebDeployementReact">
             <AppContent />
         </BrowserRouter>
     );
