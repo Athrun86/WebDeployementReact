93ed1fff6aea63bf9c8adf410cee6a2d35589527
Athrun86 <la228004@student.helha.be>
2025-11-11 14:22:17 +0100

    Remove .env from repository

--- frontend/src/api/repository.ts
+++ frontend/src/api/repository.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
+console.log(apiUrl);
+
 export async function requestvalidationToken(token: string): Promise<boolean> {
     try {
         await axios.get(`${apiUrl}/login/validate-token`, {
